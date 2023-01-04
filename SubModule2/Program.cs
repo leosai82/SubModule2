5f1cc5de0fd819729469beca31541f149ba5f028
leosai82 <75162300+leosai82@users.noreply.github.com>
2023-01-04 17:23:11 +0900

    init

---
+++ SubModule2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SubModule2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new SubModuleLib.Class2();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
